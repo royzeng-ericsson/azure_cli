94948f770697b918d1b83e12f5ef5c7253170408
Roy Zeng <roy.zeng@ericsson.com>
2018-03-09 06:03:09 +0000

    update help config

--- delete_vm.sh
+++ delete_vm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 VM_PRE_NAME=$1
 VM_NUMBER=$2
-if [ $# -lt 2 ] ; then
-    echo "Please input virtual machine prefix name and number of vms"
-elif [ $1 = "-h" -o $1 = "--help" ] ; then
+if [ $1 = "-h" -o $1 = "--help" ] ; then
     echo  "Usage: ./delete_vm.sh <vm_pre_name> <number_of_vm>"
+elif [ $# -lt 2 ] ; then
+    echo "Please input virtual machine prefix name and number of vms"
 else
     for loop in `seq 1 ${VM_NUMBER}`
     do
--- start_vm.sh
+++ start_vm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ $# -lt 2 ] ; then
-    echo "Please input virtual machine prefix name and number of vms"
-elif [ $1 = "-h" -o $1 = "--help" ] ; then
+if [ $1 = "-h" -o $1 = "--help" ] ; then
     echo  "Usage: ./start_vm.sh <vm_pre_name> <number_of_vm>"
+elif [ $# -lt 2 ] ; then
+    echo "Please input virtual machine prefix name and number of vms"
 else
     for loop in `seq 1 $2`
     do
